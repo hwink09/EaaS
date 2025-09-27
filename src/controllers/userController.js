6626a9f332b785e9f65998da0f3b1202cf2f71fc
hwink09 <caotranhoangminh2003@gmail.com>
2025-09-28 03:59:19 +0700

    Add ResendProvider for email sending and integrate with user registration

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,49 @@
+/* eslint-disable no-console */
 import { StatusCodes } from 'http-status-codes'
 import { MOCK_USER } from '~/models/mockDatabase'
+import { ResendProvider } from '~/providers/ResendProvider'
 
 const register = async (req, res) => {
   try {
     const createdUser = MOCK_USER
     // Gửi mail cho user sau khi đăng ký tài khoản, có thể là mail xác nhận, mail welcome...vv
     // Bước gửi mail này sẽ là việc gửi hành động đến một dịch vụ Email as a Service.
+    const to = createdUser.EMAIL
+    const subject = 'Created account successfully - Hwinkdev'
+    const html = `
+  <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333; padding: 20px;">
+    <div style="max-width: 600px; margin: auto; border: 1px solid #e0e0e0; border-radius: 10px; box-shadow: 0 4px 8px rgba(0,0,0,0.05);">
+      <div style="background: linear-gradient(135deg, #4f46e5, #3b82f6); padding: 20px; border-radius: 10px 10px 0 0; text-align: center; color: white;">
+        <h1 style="margin: 0; font-size: 24px;">🎉 Welcome to Hwinkdev</h1>
+      </div>
+      <div style="padding: 30px;">
+        <h2 style="margin-top: 0; color: #111;">Hello ${createdUser.USERNAME},
+        </h2>
+        <p style="font-size: 16px;">
+          Your account has been <strong style="color: #16a34a;">created successfully</strong>.
+        </p> 
+        </div>
+        <p style="font-size: 14px; color: #666;">
+          If you have any questions, feel free to reply to this email — we’re happy to help!
+        </p>
+      </div>
+      <div style="background: #f9fafb; padding: 15px; border-top: 1px solid #e0e0e0; text-align: center; border-radius: 0 0 10px 10px;">
+        <p style="margin: 0; font-size: 12px; color: #999;">
+          &copy; ${new Date().getFullYear()} Hwinkdev. All rights reserved.
+        </p>
+      </div>
+    </div>
+  </div>
+`
+
+    //Gửi mail với Resend
+    const sentEmailResponse = await ResendProvider.sendEmail({
+      to,
+      subject,
+      html
+    })
+    console.log('🚀 ~ register ~ sentEmailResponse:', sentEmailResponse)
+
     // Trả về response với thông tin user đã được tạo
     res.status(StatusCodes.OK).json(createdUser)
   } catch (error) {
